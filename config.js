fdfeb66d47d6f064bbcffa33b9cc3cacfdd130fd
yawata <koouji4696@gmail.com>
2026-08-31 16:16:18 +0900

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@
 window.APP_CONFIG = {
   // GASウェブアプリのURL（/exec で終わるもの）
   // 例: "https://script.google.com/macros/s/AKfycb.../exec"
-  API_BASE: "https://script.google.com/macros/s/XXXXXXXXXXXX/exec",
+  API_BASE: "https://script.google.com/macros/s/AKfycbxrS6jGGXNvb5gIXIgccrHvA8hpN5_Ck7iQoEpartWzGyRyJxYosFbdlkbvEQ4JLbMr/exec",
 
   // GASのスクリプトプロパティ「APP_TOKEN」と同じ文字列。
   // 目隠し程度の役割なので、適当な英数字でよい。
